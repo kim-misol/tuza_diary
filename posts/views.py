7a2c648f0f669e64b89410088c9463d5a4564113
kim-misol <wonderfulpine@gmail.com>
2021-02-27 20:04:14 +0900

    index 함수로 index.html 불러와서 post list 보여주기

--- posts/views.py
+++ posts/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 
 import websockets
 import asyncio
+from .models import Post
 
 
-# Create your views here.
+# index.html 페이지를 부르는 index 함수 (post list를 보여준다)
 def index(request):
-    return render(request, 'main/index.html')
+    postlist = Post.objects.all()
+    # blog.html 페이지를 열 때, 모든 Post인 postlist도 같이 가져옵니다
+    return render(request, 'main/index.html', {'postlist': postlist})
 
 
-# post.html 페이지 부르는 함수
+# post.html 페이지를 부르는 post 함수
 def post(request):
     return render(request, 'main/post.html')
 
@@ -38,10 +41,11 @@ async def get_crytocurrency_ticker(type="ticker", code="BTC_KRW", tick_type="30M
 
 def data_processing():
     d = {"type": "ticker", "content": {"tickType": "30M", "date": "20210227", "time": "163703", "openPrice": "54234000",
-                                   "closePrice": "54263000", "lowPrice": "54102000", "highPrice": "54519000",
-                                   "value": "5861540624.04524", "volume": "107.85047173", "sellVolume": "59.07722415",
-                                   "buyVolume": "48.77324758", "prevClosePrice": "53672000", "chgRate": "0.05",
-                                   "chgAmt": "29000", "volumePower": "82.56", "symbol": "BTC_KRW"}}
+                                       "closePrice": "54263000", "lowPrice": "54102000", "highPrice": "54519000",
+                                       "value": "5861540624.04524", "volume": "107.85047173",
+                                       "sellVolume": "59.07722415",
+                                       "buyVolume": "48.77324758", "prevClosePrice": "53672000", "chgRate": "0.05",
+                                       "chgAmt": "29000", "volumePower": "82.56", "symbol": "BTC_KRW"}}
 
 
 if __name__ == "__main__":
