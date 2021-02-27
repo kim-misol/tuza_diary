b72534c30bd58b3436594dd1bc2db628ab45a8a9
kim-misol <wonderfulpine@gmail.com>
2021-02-27 17:59:54 +0900

    post 모델 추가

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+# 게시글(Post)엔 제목(postname), 내용(contents)이 존재합니다
+class Post(models.Model):
+    postname = models.CharField(max_length=50)
+    market = models.CharField(max_length=10)
+    code_name = models.CharField(max_length=20)
+    code = models.CharField(max_length=10)
+    summary = models.TextField()
+
+    chart_data_json = models.CharField(max_length=50)
+
+    buy_price = models.FloatField()
+    buy_date = models.DateField()
+    buy_memo = models.TextField()
+
+    sell_price = models.FloatField()
+    sell_date = models.DateField()
+    sell_memo = models.TextField()
+
+    to_sell_price = models.FloatField()
+    to_sell_datee = models.DateField()
+    to_buy_price = models.FloatField()
+    to_buy_datee = models.DateField()
+
