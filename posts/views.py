34af4b3a8c48c9c0b1d7df5e60a6abe286e37368
kim-misol <wonderfulpine@gmail.com>
2021-02-27 16:31:40 +0900

    포스트 페이지 추가

--- posts/views.py
+++ posts/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+
+# post.html 페이지 부르는 함수
+def post(request):
+    return render(request, 'main/post.html')
